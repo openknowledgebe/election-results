4041e812405ea2d3192ecc775c04e10e9086bf6a
Michiel Leyman <michiel.leyman@gmail.com>
2019-05-25 12:16:13 +0200

    Remove boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      Election results
+    </div>
+  );
+}
+
+export default App;
