8cf3d42857cbddf5b2241333cbecb83a18632347
Michiel Leyman <michiel.leyman@gmail.com>
2019-05-25 14:22:07 +0200

    Get candidates on app level

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { API, ELECTION_TYPES } from '../constants';
 import Overview from './Overview';
 import Candidate from './Candidate';
 import NotFound from './NotFound';
@@ -7,15 +8,31 @@ import NotFound from './NotFound';
 import 'normalize.css';
 import '../index.css';
 
-const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" render={() => <Redirect to="/2019"/>} />
-      <Route exact path="/:year" component={Overview} />
-      <Route path="/:year/:list/:party/:candidate" component={Candidate} />
-      <Route component={NotFound} />
-    </Switch>
-  </BrowserRouter>
-);
+const App = () => {
+  const [candidates, setCandidates] = useState([]);
+
+  const getCandidates = async () => {
+    const queue = ELECTION_TYPES.map(type => API.getCandidates(type));
+    const results = await Promise.all(queue);
+    return results.reduce((accumulator, candidatesPerType) => {
+      return { ...accumulator, ...candidatesPerType };
+    }, {});
+  }
+
+  useEffect(() => {
+    getCandidates().then(setCandidates);
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to="/2019"/>} />
+        <Route exact path="/:year" render={() => <Overview candidates={candidates} />} />
+        <Route path="/:year/:list/:party/:candidate" render={() => <Candidate candidates={candidates} />} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/components/Candidate.jsx
+++ src/components/Candidate.jsx
@@ -1,23 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { API, ELECTION_TYPES } from '../constants';
+import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 const Candidate = ({ match }) => {
-  const [candidates, setCandidates] = useState([]);
-
   const { year, list, party, candidate } = match.params;
-
-
-  const getCandidates = async () => {
-    const queue = ELECTION_TYPES.map(type => API.getCandidates(type));
-    const results = await Promise.all(queue);
-    return results.reduce((accumulator, candidatesPerType) => {
-      return { ...accumulator, ...candidatesPerType };
-    }, {});
-  }
-
-  useEffect(() => {
-    getCandidates().then(setCandidates);
-  }, []);
 
   return (
     <div>
@@ -26,4 +11,4 @@ const Candidate = ({ match }) => {
   );
 };
 
-export default Candidate;
+export default withRouter(Candidate);
