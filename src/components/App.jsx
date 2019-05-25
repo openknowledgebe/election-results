33cc71b514df364c2fb53a4a5df3e827eb6f98fe
Michiel Leyman <michiel.leyman@gmail.com>
2019-05-25 12:50:39 +0200

    Redirect to 2019 by default

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Overview from './Overview';
 import Candidate from './Candidate';
 import NotFound from './NotFound';
@@ -10,6 +10,7 @@ import '../index.css';
 const App = () => (
   <BrowserRouter>
     <Switch>
+      <Route exact path="/" render={() => <Redirect to="/2019"/>} />
       <Route path="/:year" component={Overview} />
       <Route path="/:year/:list/:party/:candidate" component={Candidate} />
       <Route component={NotFound} />
