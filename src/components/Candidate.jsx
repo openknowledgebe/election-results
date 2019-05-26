e9d586813ab8c2d5df3fa593210a66db30ca85d1
Michiel Leyman <michiel.leyman@gmail.com>
2019-05-26 10:54:50 +0200

    Add party logos

--- src/components/Candidate.jsx
+++ src/components/Candidate.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import moment from 'moment';
 import { withRouter } from 'react-router-dom';
 import okbeLogo from '../assets/img/okbe-logo.png';
+import partiesLogos from '../assets/data/parties-logos.json';
+
 import '../assets/css/candidate.css';
 
 const Candidate = ({ match, electionData }) => {
@@ -49,10 +51,20 @@ const Candidate = ({ match, electionData }) => {
     return sum + evo.stations_total;
   }, 0);
 
+
+  const partyImgSrc = partiesLogos.find((p) => {
+    return p.name === candidate.list.group.name && p.ids.includes(candidate.list.group.id);
+  }) || null;
+
+  console.log(candidate, partiesLogos);
   return (
     <div className="candidate-container">
       <header>
         <div className="party-logo-container">
+          { partyImgSrc
+            ? <img src={partyImgSrc} alt={`${candidate.list.name} logo`} />
+            : <p>No logo available</p>
+          }
         </div>
         <div className="name-container" style={{ backgroundColor: `#${color}` }}>
           <h2 className="first-name">{candidateName.firstName}</h2>
